bfc385277bdf42bac12add465b2d855953fc18a2
rajohan <rajohan1@gmail.com>
2018-01-26 20:29:06 +0100

    Updates

--- classes/filter.php
+++ classes/filter.php
@@ -5,10 +5,12 @@
     class Filter {
 
         function sanitize($data) {
+
             $data = trim($data);
             $data = strip_tags($data);
             $data = htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
             return $data;
+            
         }
 
     }
