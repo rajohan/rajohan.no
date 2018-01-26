b7650d7e2b89071887dd41b127ca96be04e74be7
rajohan <rajohan1@gmail.com>
2018-01-26 20:27:18 +0100

    Updates

--- classes/filter.php
+++ classes/filter.php
@@ -4,10 +4,11 @@
     ###########################################################################
     class Filter {
 
-        function sanitize() {
+        function sanitize($data) {
             $data = trim($data);
             $data = strip_tags($data);
             $data = htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
+            return $data;
         }
 
     }
